b837ce421eef1fe950a4f9e95f997b6c608ff16d
Hidden-Bug-0x1 <gwprielipp@gmail.com>
2021-02-27 18:04:29 -0900

    Big changes

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import React, { Fragment, useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+
+function App() {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+    // es-lint-disable-next-line
+  }, []);
+
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <Logs />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
